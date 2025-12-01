f1a177e54c97a2eaeea5f3a6665b9779ccf671a8
Jelle Verheul <verheuljelle1@gmail.com>
2025-12-01 18:52:15 +0100

    Add Score class and integrate score tracking into WorldMap

---
+++ src/main/java/nl/saxion/game/game/entities/Score.java
@@ -0,0 +1,41 @@
+package nl.saxion.game.game.entities;
+
+import com.badlogic.gdx.graphics.Color;
+import nl.saxion.gameapp.GameApp;
+
+public class Score {
+
+    int positionX = GameApp.getWindowWidth() + 300;
+    int positionY = GameApp.getWindowHeight() + 150;
+    int score = 0;
+
+    float timeSinceLastAdd = 0f;
+
+    public void update(float delta) {
+        // waits until 1 second has passed then update the score.
+        timeSinceLastAdd += delta;
+        if (timeSinceLastAdd >= 1f) {
+            increaseScoreBy(1);
+            timeSinceLastAdd -= 1f;
+        }
+    }
+
+    public void render() {
+        GameApp.drawTexture("scoreBoard", positionX - 75, positionY - 75, 275, 200);
+        GameApp.setColor(Color.RED);
+        GameApp.drawRect(positionX, positionY, 100, 100);
+
+        GameApp.drawTextCentered("basicSmall", "Score:", positionX, positionY + 50, Color.CYAN);
+
+        GameApp.drawTextCentered("cyberpunk", String.valueOf(score), positionX + 125, positionY + 20, Color.CYAN);
+    }
+
+    public void increaseScoreBy(int increase) {
+        score += increase;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+}
--- src/main/java/nl/saxion/game/game/screens/WorldMap.java
+++ src/main/java/nl/saxion/game/game/screens/WorldMap.java
@@ -1,9 +1,11 @@
 package nl.saxion.game.game.screens;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import nl.saxion.game.game.entities.Player;
+import nl.saxion.game.game.entities.Score;
 import nl.saxion.gameapp.GameApp;
 import nl.saxion.gameapp.screens.ScalableGameScreen;
 import nl.saxion.game.game.entities.Enemy;
@@ -13,6 +15,7 @@ public class WorldMap extends ScalableGameScreen {
     private OrthogonalTiledMapRenderer mapRenderer;
     private Player player;
     private Enemy enemy;
+    private Score score;
     private OrthographicCamera camara;
 
     public WorldMap() {
@@ -24,6 +27,11 @@ public class WorldMap extends ScalableGameScreen {
         tiledMap = new TmxMapLoader().load("maps/test/testMap.tmx");
         mapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 
+        GameApp.addFont("cyberpunk", "fonts/Sefa.ttf", 75);
+        GameApp.addTexture("scoreBoard", "textures/scoreBoard.png");
+        GameApp.addFont("basicSmall", "fonts/basic.ttf", 50);
+
+
         camara = new OrthographicCamera();
         camara.setToOrtho(false, 320,  180);
 
@@ -33,6 +41,8 @@ public class WorldMap extends ScalableGameScreen {
         player = new Player(320, 160, tiledMap);
 
         enemy = new Enemy(350, 160, tiledMap, player);
+
+        score = new Score();
     }
 
     @Override
@@ -42,7 +52,7 @@ public class WorldMap extends ScalableGameScreen {
 
         player.update(delta);
         enemy.update(delta);
-
+        score.update(delta);
 
         camara.position.set(player.getX(), player.getY(), 0);
         camara.update();
@@ -52,8 +62,15 @@ public class WorldMap extends ScalableGameScreen {
 
         GameApp.startShapeRenderingFilled();
         GameApp.getShapeRenderer().setProjectionMatrix(camara.combined);
-        player.render();
+
+        GameApp.startSpriteRendering();
+
         enemy.render();
+        player.render();
+
+        score.render();
+
+        GameApp.endSpriteRendering();
         GameApp.endShapeRendering();
     }
 
