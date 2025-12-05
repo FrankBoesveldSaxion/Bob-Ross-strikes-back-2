31c15a0e6f7fa1916bc6f21d1a00a118fc1c7b33
Jelle Verheul <589530@student.saxion.nl>
2025-12-05 14:30:19 +0100

    Refactor player animation setup: use SpriteConfig for frame dimensions and duration

--- src/main/java/nl/saxion/game/game/entities/Player.java
+++ src/main/java/nl/saxion/game/game/entities/Player.java
@@ -1,6 +1,7 @@
 package nl.saxion.game.game.entities;
 
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import nl.saxion.game.game.systems.SpriteConfig;
 import nl.saxion.gameapp.GameApp;
 
 import static nl.saxion.game.game.systems.CollisionSystem.isCollision;
@@ -18,15 +19,12 @@ public class Player {
     }
 
     public void show() {
-        int frameWidth = 32;
-        int frameHeight = 32;
-        float frameDuration = 0.08f;
+        SpriteConfig config = new SpriteConfig();
+        GameApp.addSpriteSheet("bobWalkLeft", "textures/animations/bobRossRunAnimationLeftRun.png", config.getFrameWidth(), config.getFrameHeight());
+        GameApp.addAnimationFromSpritesheet("bobWalkLeft", "bobWalkLeft", config.getFrameDuration(), true);
 
-        GameApp.addSpriteSheet("bobWalkLeft", "textures/animations/bobRossRunAnimationLeftRun.png", frameWidth, frameHeight);
-        GameApp.addAnimationFromSpritesheet("bobWalkLeft", "bobWalkLeft", frameDuration, true);
-
-        GameApp.addSpriteSheet("bobWalkRight", "textures/animations/bobRossRunAnimationRightRun.png", frameWidth, frameHeight);
-        GameApp.addAnimationFromSpritesheet("bobWalkRight", "bobWalkRight", frameDuration, true);
+        GameApp.addSpriteSheet("bobWalkRight", "textures/animations/bobRossRunAnimationRightRun.png", config.getFrameWidth(), config.getFrameHeight());
+        GameApp.addAnimationFromSpritesheet("bobWalkRight", "bobWalkRight", config.getFrameDuration(), true);
     }
 
     public void update(float delta) {
@@ -72,12 +70,17 @@ public class Player {
     public void render() {
         // draw animation based on the direction.
         if (currentDirection == 1) {
-            GameApp.drawAnimation("bobWalkLeft", x-15, y-5, 32f, 32f);
+            GameApp.drawAnimation("bobWalkLeft", x - 15, y - 5, 32f, 32f);
         } else {
-            GameApp.drawAnimation("bobWalkRight", x-15, y-5, 32f, 32f);
+            GameApp.drawAnimation("bobWalkRight", x - 15, y - 5, 32f, 32f);
         }
     }
 
-    public float getX() { return x; }
-    public float getY() { return y; }
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
 }
---
+++ src/main/java/nl/saxion/game/game/systems/SpriteConfig.java
@@ -0,0 +1,19 @@
+package nl.saxion.game.game.systems;
+
+public class SpriteConfig {
+    int frameWidth = 32;
+    int frameHeight = 32;
+    float frameDuration = 0.08f;
+
+    public int getFrameWidth() {
+        return frameWidth;
+    }
+
+    public int getFrameHeight() {
+        return frameHeight;
+    }
+
+    public float getFrameDuration() {
+        return frameDuration;
+    }
+}
